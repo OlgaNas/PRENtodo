32e08ec00300db1603a9a50f00312839ff64cc7d
Olga Nastaja <olga.vlasova321@gmail.com>
2023-10-17 13:42:25 +0100

    database connection 2

--- server/db.js
+++ server/db.js
@@ -1,20 +1,20 @@
 const Pool = require("pg").Pool;
 require('dotenv').config();
 
-const pool = new Pool({ // create connection to database
-    connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Render app 
-    ssl: {
-      rejectUnauthorized: false // don't check for SSL cert
-    }
-  });
-  
+// const pool = new Pool({ // create connection to database
+//     connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Render app 
+//     ssl: {
+//       rejectUnauthorized: false // don't check for SSL cert
+//     }
+//   });
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_DATABASE
-// });
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
+});
 
 module.exports = pool;
